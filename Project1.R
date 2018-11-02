7f36590ee5f0bfc2a1d3a1b3486691b42e2c0c3c
Axolotl-Anonyme <kalakof_92@hotmail.fr>
2018-11-02 11:38:53 +0100

    first sending

---
+++ Project1.R
@@ -0,0 +1,11 @@
+#1. Compute standard descriptive statistics for the variables in this study
+wages <- bwages["WAGE"][1]
+wages[1]
+wages[2]
+wages_num <- as.numeric(as.vector((wages)))
+sd_stats <- matrix((mean(wages), median(wages), sd(wages), var(wages))
+
+                   
+                   
+                  
+apply(bwages,mean)
